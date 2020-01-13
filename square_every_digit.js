75bf842aeef060c363501274f60b17475d5bd31d
Crew Spence <csspence123@gmail.com>
2020-01-13 06:36:56 -0600

    Wrote working solution for square_every_digit.

--- square_every_digit.js
+++ square_every_digit.js
@@ -6,11 +6,13 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 Note: The function accepts an integer and returns an integer
 */
 
-function squareDigits(num){
-  //Restate the problem in your own words.
-  //What are the inputs?
-  //What are the outputs?
-  //Can the outputs be determined from the inputs?  Do I already have enough information to solve the problem?  What if you don't have enough information from the user?
-  //How should you label the important pieces of data that are a part of the problem?
-    
+const squareDigits = (num) => {
+  const number = num.toString();
+  let answer = '';
+  for(let i = 0; i < number.length; i++) {
+    let digit = parseInt(number[i]);
+    answer += digit * digit;
+  }
+  
+  return parseInt(answer);
   }
