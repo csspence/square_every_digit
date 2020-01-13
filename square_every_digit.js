f13179a3d497a72fd1976f5ac9d64aeba39f8de1
Crew Spence <csspence123@gmail.com>
2020-01-13 06:26:23 -0600

    Created function stub for Code Wars toy problem.

---
+++ square_every_digit.js
@@ -0,0 +1,16 @@
+/*
+Welcome. In this kata, you are asked to square every digit of a number.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+function squareDigits(num){
+  //Restate the problem in your own words.
+  //What are the inputs?
+  //What are the outputs?
+  //Can the outputs be determined from the inputs?  Do I already have enough information to solve the problem?  What if you don't have enough information from the user?
+  //How should you label the important pieces of data that are a part of the problem?
+    
+  }
